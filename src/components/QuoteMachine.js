69c4b13ba4cec84cc8b31af965ca8691d1e3bde4
soumit-git <soumit.goswami12@gmail.com>
2019-02-10 20:48:15 +0530

    Initial commit

---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import '.././bootstrap-social.css'
+
+class QuoteMachine extends React.Component{
+  render(){
+    return(
+      <Card style={{width:'36rem'}} className="text-center">
+        <Card.Header>Quote</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p>
+              {' '}{this.props.quote}{' '}
+            </p>
+          <footer className="blockquote-footer">
+          {this.props.author}
+          </footer>
+          </blockquote><br />
+          <Button variant="primary" active onClick={this.props.newQuote}>New Quote</Button>{' '}
+          <a href={this.props.link} className="btn btn-social-icon btn-twitter"><span class="fa fa-twitter"></span></a>
+          </Card.Body>
+        </Card>
+    )
+  }
+}
+
+export default QuoteMachine;
